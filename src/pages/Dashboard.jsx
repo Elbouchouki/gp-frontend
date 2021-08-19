41f5b55a488a5a459557e4fd4ec3796d92712cab
Elbouchouki <elbouchoukigamer@gmail.com>
2021-08-19 01:06:04 +0100

    starting dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,356 +1,143 @@
 import React, { useEffect, useState } from 'react'
-
-import { Link } from 'react-router-dom'
-
 import Chart from 'react-apexcharts'
-
 import { useSelector } from 'react-redux'
-
 import StatusCard from '../components/status-card/StatusCard'
-import customerList from '../assets/JsonData/customers-list.json'
+import { Nav} from 'rsuite'
 
-import Table from '../components/table/Table'
-
-import Badge from '../components/badge/Badge'
-
-import statusCards from '../assets/JsonData/status-card-data.json'
-import { addDays, subDays } from 'date-fns'
-import { DateRangePicker } from 'rsuite'
-import { Nav, Icon, FlexboxGrid, DatePicker } from 'rsuite'
-const chartOptions = {
-    series: [{
+const options= {
+    color: ['#6ab04c', '#2980b9'],
+    chart: {
+        background: 'transparent'
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        curve: 'smooth'
+    },
+    xaxis: {
+        categories: ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Juil', 'Aout', 'Sep','Oct','Nov','Dec']
+    },
+    legend: {
+        position: 'bottom'
+    },
+    grid: {
+        show: false
+    }
+}
+const statusCards= [
+    {
+        icon: "bx bx-shopping-bag",
+        count: "$995",
+        title: "Tickets normals"
+    },
+    {
+        icon: "bx bx-cart",
+        count: "$214,251",
+        title: "Tickets illisibles"
+    },
+    {
+        icon: "bx bx-dollar-circle",
+        count: "$13,632",
+        title: "Tickets perdus"
+    },
+    {
+        icon: "bx bx-receipt",
+        count: "1,711",
+        title: "Total normals"
+    }, {
+        icon: "bx bx-shopping-bag",
+        count: "1,995",
+        title: "Total illisibles"
+    },
+    {
+        icon: "bx bx-cart",
+        count: "801",
+        title: "Total perdus"
+    }
+]
+const CustomNav = ({ active, onSelect, ...props }) => {
+    return (
+        <Nav  {...props} activeKey={active} onSelect={onSelect} >
+            <Nav.Item pullRight eventKey={1}>Jour</Nav.Item>
+            <Nav.Item eventKey={2}>Semaine</Nav.Item>
+            <Nav.Item eventKey={3}>Mois</Nav.Item>
+            <Nav.Item eventKey={4}>Année</Nav.Item>
+        </Nav>
+    );
+};
+const Dashboard = () => {
+    const [yearChart,setYearChart] = useState([{
         name: 'Online Customers',
         data: [40, 70, 20, 90, 36, 80, 30, 91, 60]
     }, {
         name: 'Store Customers',
-        data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10]
-    }],
-    options: {
-        color: ['#6ab04c', '#2980b9'],
-        chart: {
-            background: 'transparent'
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {
-            curve: 'smooth'
-        },
-        xaxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
-        },
-        legend: {
-            position: 'top'
-        },
-        grid: {
-            show: false
-        }
-    }
-}
-
-const topCustomers = {
-    head: [
-        'user',
-        'total orders',
-        'total spending'
-    ],
-    body: [
-        {
-            "username": "john doe",
-            "order": "490",
-            "price": "$15,870"
-        },
-        {
-            "username": "frank iva",
-            "order": "250",
-            "price": "$12,251"
-        },
-        {
-            "username": "anthony baker",
-            "order": "120",
-            "price": "$10,840"
-        },
-        {
-            "username": "frank iva",
-            "order": "110",
-            "price": "$9,251"
-        },
-        {
-            "username": "anthony baker",
-            "order": "80",
-            "price": "$8,840"
-        }
-    ]
-}
-
-const renderCusomerHead = (item, index) => (
-    <th key={index}>{item}</th>
-)
-
-const renderCusomerBody = (item, index) => (
-    <tr key={index}>
-        <td>{item.username}</td>
-        <td>{item.order}</td>
-        <td>{item.price}</td>
-    </tr>
-)
-
-const latestOrders = {
-    header: [
-        "order id",
-        "user",
-        "total price",
-        "date",
-        "status"
-    ],
-    body: [
-        {
-            id: "#OD1711",
-            user: "john doe",
-            date: "17 Jun 2021",
-            price: "$900",
-            status: "shipping"
-        },
-        {
-            id: "#OD1712",
-            user: "frank iva",
-            date: "1 Jun 2021",
-            price: "$400",
-            status: "paid"
-        },
-        {
-            id: "#OD1713",
-            user: "anthony baker",
-            date: "27 Jun 2021",
-            price: "$200",
-            status: "pending"
-        },
-        {
-            id: "#OD1712",
-            user: "frank iva",
-            date: "1 Jun 2021",
-            price: "$400",
-            status: "paid"
-        },
-        {
-            id: "#OD1713",
-            user: "anthony baker",
-            date: "27 Jun 2021",
-            price: "$200",
-            status: "refund"
-        }
-    ]
-}
-
-const orderStatus = {
-    "shipping": "primary",
-    "pending": "warning",
-    "paid": "success",
-    "refund": "danger"
-}
-
-const renderOrderHead = (item, index) => (
-    <th key={index}>{item}</th>
-)
-
-const renderOrderBody = (item, index) => (
-    <tr key={index}>
-        <td>{item.id}</td>
-        <td>{item.user}</td>
-        <td>{item.price}</td>
-        <td>{item.date}</td>
-        <td>
-            <Badge type={orderStatus[item.status]} content={item.status} />
-        </td>
-    </tr>
-)
-const customerTableHead = [
-    '',
-    'name',
-    'email',
-    'phone',
-    'total orders',
-    'total spend',
-    'location'
-]
-
-const renderHead = (item, index) => <th key={index}>{item}</th>
-
-const renderBody = (item, index) => (
-    <tr key={index}>
-        <td>{item.id}</td>
-        <td>{item.name}</td>
-        <td>{item.email}</td>
-        <td>{item.phone}</td>
-        <td>{item.total_orders}</td>
-        <td>{item.total_spend}</td>
-        <td>{item.location}</td>
-    </tr>
-)
-const styles = {
-    marginBottom: 50
-};
-const CustomNav = ({ active, onSelect, ...props }) => {
-    return (
-        <Nav  {...props} activeKey={active} onSelect={onSelect} style={styles}>
-            <Nav.Item eventKey="day">Jour</Nav.Item>
-            <Nav.Item eventKey="week">Semaine</Nav.Item>
-            <Nav.Item eventKey="month">Mois</Nav.Item>
-            <Nav.Item eventKey="year">Année</Nav.Item>
-        </Nav>
-    );
-};
-
-const DatePickerWeekDate = ({ active, handleDateChange }) => {
-    return (<DateRangePicker cleanable={false}
-        onChange={(value) => { handleDateChange(value) }}
-        oneTap preventOverflow
-        hoverRange={active}
-        showWeekNumbers={active === 'week' ? true : false}
-        placeholder="Choisir une date"
-        ranges={[]}
-        format="DD/MM/YYYY"
-        locale={{
-            sunday: 'Dim',
-            monday: 'Lun',
-            tuesday: 'Mar',
-            wednesday: 'Mer',
-            thursday: 'Jeu',
-            friday: 'Ven',
-            saturday: 'Sam',
-            ok: 'OK',
-            today: "Aujourd'hui",
-            yesterday: 'Hier',
-            last7Days: 'Last 7 days'
-        }}
-    />)
-}
-const Dashboard = () => {
-
+        data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10,1,1]
+    }])
+    const [loading,setLoading] = useState(true)
     const themeReducer = useSelector(state => state.ThemeReducer.mode)
-    const [active, setActive] = useState('day')
-    const [fromDate, setFromDate] = useState(null)
-    const [toDate, setToDate] = useState(null)
-    const handleDateChange = (value) => {
-        setFromDate(value[0])
-        setToDate(value[1])
-        console.log(fromDate)
-    }
+    const [active, setActive] = useState(1)
     const handleSelect = (activeKey) => {
         setActive(activeKey);
     }
     return (
         <div>
-            <h2 className="page-header">Dashboard</h2>
+            
+                <div className="row" style={{justifyContent:"space-between",margin:10,alignItems:"center"}} > 
+                    <h2 className="page-header">Accueil</h2>
+                    <CustomNav appearance="subtle" active={active} onSelect={handleSelect} />
+                </div>
             <div className="row">
-                <div className="col-6">
+                <div className="col-4">
                     <div className="row">
-                        {
-                            statusCards.map((item, index) => (
-                                <div className="col-6" key={index}>
+                        <div className="col-12">
                                     <StatusCard
-                                        icon={item.icon}
-                                        count={item.count}
-                                        title={item.title}
-                                    />
-                                </div>
-                            ))
-                        }
+                                        icon="bx bx-receipt"
+                                        count="1234"
+                                        title="Tickets"
+                                        />
+                        </div>
+                        <div className="col-12">
+                                    <StatusCard
+                                        icon="bx bx-receipt"
+                                        count="$13123"
+                                        title="Revenue"
+                                        />
+                        </div>
+                        <div className="col-12">
+                                    <StatusCard
+                                        icon="bx bxs-traffic-barrier"
+                                        count="$13123"
+                                        title="Mouvements"
+                                        />
+                        </div>
+                        
                     </div>
                 </div>
-                <div className="col-6">
+                <div className="col-8">
                     <div className="card full-height">
                         {/* chart */}
                         <Chart
                             options={themeReducer === 'theme-mode-dark' ? {
-                                ...chartOptions.options,
+                                ...options,
                                 theme: { mode: 'dark' }
                             } : {
-                                ...chartOptions.options,
+                                ...options,
                                 theme: { mode: 'light' }
                             }}
-                            series={chartOptions.series}
+                            series={yearChart}
                             type='line'
                             height='100%'
                         />
                     </div>
                 </div>
-                {/* <div className="col-4">
-                    <div className="card">
-                        <div className="card__header">
-                            <h3>top customers</h3>
-                        </div>
-                        <div className="card__body">
-                            <Table
-                                headData={topCustomers.head}
-                                renderHead={(item, index) => renderCusomerHead(item, index)}
-                                bodyData={topCustomers.body}
-                                renderBody={(item, index) => renderCusomerBody(item, index)}
-                            />
-                        </div>
-                        <div className="card__footer">
-                            <Link to='/'>view all</Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-8">
-                    <div className="card">
-                        <div className="card__header">
-                            <h3>latest orders</h3>
-                        </div>
-                        <div className="card__body">
-                            <Table
-                                headData={latestOrders.header}
-                                renderHead={(item, index) => renderOrderHead(item, index)}
-                                bodyData={latestOrders.body}
-                                renderBody={(item, index) => renderOrderBody(item, index)}
-                            />
-                        </div>
-                        <div className="card__footer">
-                            <Link to='/'>view all</Link>
-                        </div>
-                    </div>
-                </div> */}
-
                 <div className="col-12">
 
                     <div className="card">
                         <div className="card__header">
-                            <FlexboxGrid justify="space-between">
-                                {active === 'week' || active === 'month' ?
-                                    <DatePickerWeekDate active={active} handleDateChange={handleDateChange} />
-                                    :
-                                    <DatePicker
-                                        format="DD/MM/YYYY"
-                                        locale={{
-                                            sunday: 'Dim',
-                                            monday: 'Lun',
-                                            tuesday: 'Mar',
-                                            wednesday: 'Mer',
-                                            thursday: 'Jeu',
-                                            friday: 'Ven',
-                                            saturday: 'Sam',
-                                            ok: 'OK',
-                                            today: "Aujourd'hui",
-                                            yesterday: 'Hier',
-                                            last7Days: 'Last 7 days'
-                                        }}
-                                        oneTap cleanable={false} defaultValue={new Date()} placeholder="Select Date" />}
-
-                                <CustomNav appearance="subtle" active={active} onSelect={handleSelect} />
-                            </FlexboxGrid>
-                        </div>
-                        <div className="card__body">
-                            <Table
-                                limit='10'
-                                headData={customerTableHead}
-                                renderHead={(item, index) => renderHead(item, index)}
-                                bodyData={customerList}
-                                renderBody={(item, index) => renderBody(item, index)}
-                            />
+                           
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
